1971844f8f3044202a0b4113f4a08c51d00798fb
SemTheLegend <semmathe@gmail.com>
2023-01-16 10:14:48 -0800

    modified my files

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -1,26 +1,23 @@
 #!/usr/bin/python3
+"""Accessinh a REST API for todo lists of employees"""
 
 import requests
 import sys
 
-num = int(sys.argv[1])
 
+if __name__ == '__main__':
+    num = int(sys.argv[1])
 
-def get_todo(num):
-    """
-    Retrieves information about an employee TODO list
-
-    param1: n - requests for employee users json
-    param2: r - requests for all employee tasks json
-    param3: num - retrieves input from the commandline
-    """
     n = requests.get('https://jsonplaceholder.typicode.com/users')
     r = requests.get('https://jsonplaceholder.typicode.com/todos')
+    
     r_list = r.json()
     n_list = n.json()
+    
     completed_tasks = 0
     max_task = 0
     name = ""
+    
     for i in n_list:
         if i['id'] == num:
             name = i['name']
@@ -36,7 +33,4 @@ def get_todo(num):
     print(f'Employee {name} is done with tasks({completed_tasks}/{max_task}):')
     for i in r_list:
         if i['userId'] == num and i['completed'] == True:
-            print(i['title'])
-
-
-get_todo(num)
+            print('\t', i['title'])
