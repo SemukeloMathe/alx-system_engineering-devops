77141aae7857651268608d80c8f15c1e52b08cc2
SemTheLegend <semmathe@gmail.com>
2023-01-16 10:18:12 -0800

    0-gather_data_from_an_API

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -1,36 +1,31 @@
 #!/usr/bin/python3
-"""Accessinh a REST API for todo lists of employees"""
+"""Accessing a REST API for todo lists of employees"""
 
 import requests
 import sys
 
 
 if __name__ == '__main__':
-    num = int(sys.argv[1])
+    employeeId = sys.argv[1]
+    baseUrl = "https://jsonplaceholder.typicode.com/users"
+    url = baseUrl + "/" + employeeId
 
-    n = requests.get('https://jsonplaceholder.typicode.com/users')
-    r = requests.get('https://jsonplaceholder.typicode.com/todos')
-    
-    r_list = r.json()
-    n_list = n.json()
-    
-    completed_tasks = 0
-    max_task = 0
-    name = ""
-    
-    for i in n_list:
-        if i['id'] == num:
-            name = i['name']
+    response = requests.get(url)
+    employeeName = response.json().get('name')
 
-    for i in r_list:
-        if i['userId'] == num:
-            max_task += 1
+    todoUrl = url + "/todos"
+    response = requests.get(todoUrl)
+    tasks = response.json()
+    done = 0
+    done_tasks = []
 
-    for i in r_list:
-        if i['userId'] == num and i['completed'] == True:
-            completed_tasks += 1
+    for task in tasks:
+        if task.get('completed'):
+            done_tasks.append(task)
+            done += 1
 
-    print(f'Employee {name} is done with tasks({completed_tasks}/{max_task}):')
-    for i in r_list:
-        if i['userId'] == num and i['completed'] == True:
-            print('\t', i['title'])
+    print("Employee {} is done with tasks({}/{}):"
+          .format(employeeName, done, len(tasks)))
+
+    for task in done_tasks:
+        print("\t {}".format(task.get('title')))
